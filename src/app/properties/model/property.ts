f8e1eeb6c490d1d7da94deb685c7dec979a48932
diegoda04 <u20201c334@upc.edu.pe>
2023-12-17 12:11:03 -0500

    feat(property): added Property model for backend data reception

---
+++ src/app/properties/model/property.ts
@@ -0,0 +1,27 @@
+export interface Property {
+  id: number;
+  blockNumber: string;
+  street: string;
+  maximumFloorLevel: number;
+  yearCompleted: number;
+  residentialPropertyTag: string;
+  commercialPropertyTag: string;
+  marketAndHawkerTag: string;
+  miscellaneous: string;
+  multiStoreyCarParkTag: string;
+  precinctPavilionTag: string;
+  town: string;
+  totalDwellingUnits: number;
+  numberOneRoomSoldFlats: number;
+  numberTwoRoomSoldFlats: number;
+  numberThreeRoomSoldFlats: number;
+  numberFourRoomSoldFlats: number;
+  numberFiveRoomSoldFlats: number;
+  numberExecutiveSoldFlats: number;
+  numberMultiGenerationSoldFlats: number;
+  numberStudioApartmentSoldFlats: number;
+  numberOneRoomRentalFlats: number;
+  numberTwoRoomRentalFlats: number;
+  numberThreeRoomRentalFlats: number;
+  numberOtherRoomRentalFlats: number;
+}
