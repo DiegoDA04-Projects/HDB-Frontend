3305d0ebb7cb0f3abeecf48dc62dd95dbf9c21f3
diegoda04 <u20201c334@upc.edu.pe>
2023-12-17 22:06:05 -0500

    feat(table): created table for properties data
    - Use Table for Material UI Design
    - Implement pagination and sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {PropertiesComponent} from "./properties/pages/properties/properties.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PropertiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/properties/pages/properties/properties.component.ts
+++ src/app/properties/pages/properties/properties.component.ts
@@ -1,12 +1,121 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {Property} from "../../model/property";
+import {PropertyService} from "../../services/property.service";
+import {MatSort, MatSortModule} from "@angular/material/sort";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+
+const columns = [
+  {
+    columnDef: 'id',
+    header: "ID",
+  },
+  {
+    columnDef: 'blockNumber',
+    header: "Block Number",
+  },
+  {
+    columnDef: 'street',
+    header: 'Street',
+  },
+  {
+    columnDef: 'maximumFloorLevel',
+    header: 'Maximum Floor Level',
+  },
+  {
+    columnDef: 'yearCompleted',
+    header: 'Year Completed',
+  },
+  {
+    columnDef: 'residentialPropertyTag',
+    header: 'Residential Property Tag',
+  },
+  {
+    columnDef: 'commercialPropertyTag',
+    header: 'Commercial Property Tag',
+  },
+  {
+    columnDef: 'marketAndHawkerTag',
+    header: 'Market and Hawker Tag',
+  },
+  {
+    columnDef: 'miscellaneous',
+    header: 'Miscellaneous',
+  },
+  {
+    columnDef: 'multiStoreyCarParkTag',
+    header: 'Multi Storey Car Park Tag',
+  },
+  {
+    columnDef: 'precinctPavilionTag',
+    header: 'Precinct Pavilion Tag',
+  },
+  {
+    columnDef: 'town',
+    header: 'Town',
+  },
+  {
+    columnDef: 'totalDwellingUnits',
+    header: 'Total Dwelling Units',
+  },
+  {
+    columnDef: 'numberOneRoomSoldFlats',
+    header: 'Number One Room Sold Flats',
+  },
+  {
+    columnDef: 'numberTwoRoomSoldFlats',
+    header: 'Number Two Room Sold Flats',
+  },
+  {
+    columnDef: 'numberThreeRoomSoldFlats',
+    header: 'Number Three Room Sold Flats',
+  },
+
+]
 
 @Component({
   selector: 'app-properties',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
   templateUrl: './properties.component.html',
   styleUrl: './properties.component.css'
 })
-export class PropertiesComponent {
 
+
+export class PropertiesComponent implements OnInit, AfterViewInit {
+
+  propertyData: any;
+  datasource: MatTableDataSource<Property>;
+
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false}) sort!: MatSort;
+
+  displayedColumns = columns.map(c => c.columnDef);
+
+  constructor(private propertyService: PropertyService) {
+    this.propertyData = {} as Property;
+    this.datasource = new MatTableDataSource<any>();
+  }
+
+  ngOnInit(): void {
+    this.getAllProperties();
+  }
+
+  ngAfterViewInit() {
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+
+  }
+
+  private getAllProperties() {
+    this.propertyService.getAll().subscribe((data: any) => {
+      this.datasource.data = data.content;
+    });
+  }
+
+  protected readonly columns = columns;
 }
