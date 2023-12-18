5161c7d935b8c5965da6743fc2385de36103dc3b
diegoda04 <u20201c334@upc.edu.pe>
2023-12-18 02:11:28 -0500

    feat: added toolbar component and options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {PropertiesComponent} from "./properties/pages/properties/properties.component";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PropertiesComponent],
+  imports: [CommonModule, RouterOutlet, PropertiesComponent, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hdb-frontend';
+
+  options = [
+    { path: '/home', title: 'Home'},
+    { path: '/management/properties', title: 'Properties'},
+  ]
 }
