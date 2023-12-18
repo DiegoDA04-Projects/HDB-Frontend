39528c9e36de761275f911354faefea723aa922e
diegoda04 <u20201c334@upc.edu.pe>
2023-12-18 11:09:23 -0500

    feat(routes): added routes for HomeComponent and PageNotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import {PropertiesComponent} from "./properties/pages/properties/properties.component";
+import {HomeComponent} from "./public/pages/home/home.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'management/properties', component: PropertiesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
