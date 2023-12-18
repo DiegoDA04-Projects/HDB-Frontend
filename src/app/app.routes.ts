ecac7d4a59f3473e3b796332ffb32213b7914799
diegoda04 <u20201c334@upc.edu.pe>
2023-12-18 02:12:01 -0500

    feat: added paths for routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {PropertiesComponent} from "./properties/pages/properties/properties.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'management/properties', component: PropertiesComponent },
+];
